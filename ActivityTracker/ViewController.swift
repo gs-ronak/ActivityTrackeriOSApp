ebbf43fdc320c0804e9a6c5123fc26be42acca20
Rakshit <mac@rakshits-macbook-air.local>
2016-07-30 09:07:28 +0530

    Initial Commit

---
+++ ActivityTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActivityTracker
+//
+//  Created by Rakshit  on 30/07/16.
+//  Copyright © 2016 Zimpa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
